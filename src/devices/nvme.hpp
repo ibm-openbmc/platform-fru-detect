678cc41cdb0c90a7e45c336b076c5933ccb29d0f
Andrew Jeffery <andrew@aj.id.au>
2021-10-11 09:24:42 +1030

    nvme: Reorder method declarations
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- src/devices/nvme.hpp
+++ src/devices/nvme.hpp
@@ -14,6 +14,8 @@ class Williwakas;
 class NVMeDrive
 {
   public:
+    static bool isPresent(SysfsI2CBus bus);
+
     NVMeDrive(const NVMeDrive& drive) :
         inventory(drive.inventory), backplane(drive.backplane),
         index(drive.index)
@@ -22,12 +24,10 @@ class NVMeDrive
         inventory(inventory), backplane(backplane), index(index)
     {}
 
-    static bool isPresent(SysfsI2CBus bus);
-    std::filesystem::path getEEPROMDevicePath() const;
-    SysfsI2CDevice getEEPROMDevice() const;
-
     int probe();
 
+    std::filesystem::path getEEPROMDevicePath() const;
+    SysfsI2CDevice getEEPROMDevice() const;
     std::string getInventoryPath() const;
     std::array<uint8_t, 2> getSerial() const;
 
