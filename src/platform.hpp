441110e4fba8425a7052281a10d70a0f5c4b9279
Andrew Jeffery <andrew@aj.id.au>
2021-10-14 15:15:28 +1030

    platform: Add PolledGPIODevicePresence implementation of NotifySink
    Often we have GPIO expanders hanging off I2C and don't get interrupts
    from the lines as a result. Add a NotifySink that will poll the values
    periodically.
    
    Note this isn't very efficient at all right now. Make it work, then make
    it fast and all.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- src/platform.hpp
+++ src/platform.hpp
@@ -1,10 +1,23 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 #pragma once
 
+#include "notify.hpp"
+
+#include <gpiod.hpp>
+#include <phosphor-logging/lg2.hpp>
+
+#include <cassert>
+#include <cerrno>
+#include <cstring>
 #include <functional>
 #include <optional>
 #include <stdexcept>
-#include <string>
+#include <type_traits>
+
+extern "C"
+{
+#include <sys/timerfd.h>
+}
 
 class Inventory;
 
@@ -69,6 +82,122 @@ class Device
 
     virtual void plug() = 0;
     virtual void unplug(int mode = UNPLUG_REMOVES_INVENTORY) = 0;
+};
+
+template <typename T>
+concept DerivesDevice = std::is_base_of<Device, T>::value;
+
+/*
+ * TODO: Add multiple instances to a single timerfd to make it more efficient
+ *
+ * Haven't done this yet as I already have enough to think about to get it all
+ * to work.
+ */
+template <DerivesDevice T>
+class PolledGPIODevicePresence : public NotifySink
+{
+  public:
+    PolledGPIODevicePresence() = default;
+    PolledGPIODevicePresence(gpiod::line* line, Connector<T>* connector) :
+        line(line), connector(connector), timerfd(-1)
+    {}
+    ~PolledGPIODevicePresence() = default;
+
+    PolledGPIODevicePresence<T>&
+        operator=(const PolledGPIODevicePresence<T>& other) = default;
+
+    /* NotifySink */
+    virtual void arm() override
+    {
+        assert(timerfd == -1 && "Bad state: timer already armed");
+
+        timerfd = ::timerfd_create(CLOCK_MONOTONIC, 0);
+        if (timerfd == -1)
+        {
+            lg2::error("Failed to create timerfd: {ERRNO_DESCRIPTION}",
+                       "ERRNO_DESCRIPTION", ::strerror(errno), "ERRNO", errno);
+            std::system_category().default_error_condition(errno);
+        }
+
+        struct itimerspec interval
+        {
+            {1, 0},
+            {
+                1, 0
+            }
+        };
+
+        int rc = ::timerfd_settime(timerfd, 0, &interval, nullptr);
+        if (rc == -1)
+        {
+            close(timerfd);
+
+            lg2::error(
+                "Failed to set interval for timerfd: {ERRNO_DESCRIPTION}",
+                "ERRNO_DESCRIPTION", ::strerror(errno), "ERRNO", errno);
+            std::system_category().default_error_condition(errno);
+        }
+    }
+
+    virtual int getFD() override
+    {
+        return timerfd;
+    }
+
+    virtual void notify(Notifier& notifier) override
+    {
+        uint64_t res;
+        ssize_t rc;
+
+        rc = ::read(timerfd, &res, sizeof(res));
+        if (rc != sizeof(res))
+        {
+            if (rc == -1)
+            {
+                lg2::error(
+                    "Failed to read from timerfd {TIMER_FD}: {ERRNO_DESCRIPTION}",
+                    "TIMER_FD", timerfd, "ERRNO_DESCRIPTION", ::strerror(errno),
+                    "ERRNO", errno);
+                std::system_category().default_error_condition(errno);
+            }
+            else
+            {
+                lg2::warning(
+                    "Short read from timerfd {TIMER_FD}: {READ_LENGTH}",
+                    "TIMER_FD", timerfd, "READ_LENGTH", rc);
+            }
+        }
+
+        if (line->get_value())
+        {
+            if (!connector->isPopulated())
+            {
+                lg2::debug("Presence GPIO asserted with unpopulated connector");
+                connector->populate();
+                connector->getDevice().plug(notifier);
+            }
+        }
+        else
+        {
+            if (connector->isPopulated())
+            {
+                lg2::debug("Presence GPIO deasserted with populated connector");
+                connector->getDevice().unplug(notifier);
+                connector->depopulate();
+            }
+        }
+    }
+
+    virtual void disarm() override
+    {
+        assert(timer > -1 && "Bad state: Timer already disarmed");
+        ::close(timerfd);
+    }
+
+  private:
+    gpiod::line* line;
+    Connector<T>* connector;
+    int timerfd;
 };
 
 class Platform
