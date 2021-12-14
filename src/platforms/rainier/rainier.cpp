ibm-openbmc/platform-fru-detect

commit 19c2bec79ec41c28505cc9c88ed7f6dfc260adfa

inventory: Push down PublishWhenPresentInventoryDecorator
Use of the decorator is specific to the platform implementation.

Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

src/platforms/rainier/rainier.cpp
@@ -3,6 +3,8 @@
 
 #include "platforms/rainier.hpp"
 
+#include "inventory.hpp"
+
 void Rainier0z::enrollWith(PlatformManager& pm)
 {
     pm.enrollPlatform("Rainier 1S4U Pass 1", this);
@@ -34,8 +36,9 @@ void Rainier1z::enrollWith(PlatformManager& pm)
 
 void Rainier1z::detectFrus(Notifier& notifier, Inventory* inventory)
 {
-    Nisqually1z nisqually(inventory);
-    Ingraham ingraham(inventory, &nisqually);
+    PublishWhenPresentInventoryDecorator decoratedInventory(inventory);
+    Nisqually1z nisqually(&decoratedInventory);
+    Ingraham ingraham(&decoratedInventory, &nisqually);
 
     /* Cold-plug devices */
     ingraham.plug(notifier);

src/platform-fru-detect.cpp
@@ -47,9 +47,8 @@ int main(void)
 
     sdbusplus::bus::bus dbus = sdbusplus::bus::new_default();
     InventoryManager inventory(dbus);
-    PublishWhenPresentInventoryDecorator decoratedInventory(&inventory);
 
-    em.run(pm, &decoratedInventory);
+    em.run(pm, &inventory);
 
     return 0;
 }

commit 2191a482e6e3f565dbf659c001c79958046a9c08

rainier: Fix 0z inventory handling
Ensure we use the PublishWhenPresentInventoryDecorator to account for
the connector GPIO presence vs basic endpoint presence.

Fixes: 19c2bec79ec4 ("inventory: Push down PublishWhenPresentInventoryDecorator")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

src/platforms/rainier/rainier.cpp
@@ -14,7 +14,8 @@ void Rainier0z::enrollWith(PlatformManager& pm)
 
 void Rainier0z::detectFrus(Notifier& notifier, Inventory* inventory)
 {
-    Nisqually0z nisqually(inventory);
+    PublishWhenPresentInventoryDecorator decoratedInventory(inventory);
+    Nisqually0z nisqually(&decoratedInventory);
     Ingraham ingraham(inventory, &nisqually);
 
     /* Cold-plug devices */