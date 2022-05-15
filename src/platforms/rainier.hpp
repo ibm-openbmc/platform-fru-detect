ibm-openbmc/platform-fru-detect

commit 44f393796607d3a2082d8fa39054daf1584320b4

inventory: Refactor in terms of interfaces
Remove Inventory::updateObject() from the interface and replace it with
Inventory::add() and Inventory::remove() in terms of the Interface
subclasses. With this, we no-longer lose information about how to manage
properties associated with a specific interface, we can add and remove
them as necessary. This is fundamental for the correct operation
of PublishWhenPresentInventoryDecorator which now removes any data
interfaces when the item is marked as absent.

Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

src/platforms/rainier.hpp
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "devices/nvme.hpp"
+#include "inventory.hpp"
 #include "notify.hpp"
 #include "platform.hpp"
 #include "sysfs/i2c.hpp"
@@ -52,6 +53,8 @@ class FlettNVMeDrive : public BasicNVMeDrive
 
     const Nisqually* nisqually;
     const Flett* flett;
+    const inventory::interfaces::I2CDevice basic;
+    const inventory::interfaces::VINI vini;
 };
 
 class Flett : public Device

src/inventory.hpp
@@ -5,6 +5,7 @@
 #include "dbus.hpp"
 
 #include <functional>
+#include <list>
 #include <map>
 #include <set>
 #include <string>
@@ -37,9 +38,6 @@ static constexpr auto INVENTORY_DECORATOR_I2CDEVICE_IFACE =
     "xyz.openbmc_project.Inventory.Decorator.I2CDevice";
 
 static constexpr auto INVENTORY_IPZVPD_VINI_IFACE = "com.ibm.ipzvpd.VINI";
-
-void accumulate(std::map<std::string, inventory::ObjectType>& store,
-                const std::string& path, const inventory::ObjectType& updates);
 
 namespace interfaces
 {
@@ -128,8 +126,10 @@ class Inventory
     virtual void removePropertiesChangedListener(
         std::weak_ptr<dbus::PropertiesChangedListener> listener) = 0;
 
-    virtual void updateObject(const std::string& path,
-                              const inventory::ObjectType& updates) = 0;
+    virtual void add(const std::string& path,
+                     const inventory::interfaces::Interface iface) = 0;
+    virtual void remove(const std::string& path,
+                        const inventory::interfaces::Interface iface) = 0;
     virtual void markPresent(const std::string& path) = 0;
     virtual void markAbsent(const std::string& path) = 0;
     virtual bool isPresent(const std::string& path) = 0;
@@ -151,8 +151,10 @@ class InventoryManager : public Inventory
             override;
     virtual void removePropertiesChangedListener(
         std::weak_ptr<dbus::PropertiesChangedListener> listener) override;
-    virtual void updateObject(const std::string& path,
-                              const inventory::ObjectType& updates) override;
+    virtual void add(const std::string& path,
+                     const inventory::interfaces::Interface iface) override;
+    virtual void remove(const std::string& path,
+                        const inventory::interfaces::Interface iface) override;
     virtual void markPresent(const std::string& path) override;
     virtual void markAbsent(const std::string& path) override;
     virtual bool isPresent(const std::string& path) override;
@@ -160,6 +162,9 @@ class InventoryManager : public Inventory
                          const std::string& model) override;
 
   private:
+    virtual void updateObject(const std::string& path,
+                              const inventory::ObjectType& updates);
+
     sdbusplus::bus::bus& dbus;
     std::set<std::shared_ptr<dbus::PropertiesChangedListener>> listeners;
 };
@@ -179,8 +184,10 @@ class PublishWhenPresentInventoryDecorator : public Inventory
             override;
     virtual void removePropertiesChangedListener(
         std::weak_ptr<dbus::PropertiesChangedListener> listener) override;
-    virtual void updateObject(const std::string& path,
-                              const inventory::ObjectType& updates) override;
+    virtual void add(const std::string& path,
+                     const inventory::interfaces::Interface iface) override;
+    virtual void remove(const std::string& path,
+                        const inventory::interfaces::Interface iface) override;
     virtual void markPresent(const std::string& path) override;
     virtual void markAbsent(const std::string& path) override;
     virtual bool isPresent(const std::string& path) override;
@@ -189,6 +196,7 @@ class PublishWhenPresentInventoryDecorator : public Inventory
 
   private:
     Inventory* inventory;
-    std::map<std::string, inventory::ObjectType> objectCache;
+    std::map<std::string, std::list<inventory::interfaces::Interface>>
+        objectCache;
     std::map<std::string, bool> presentCache;
 };

src/inventory/inventory-manager.cpp
@@ -64,6 +64,26 @@ void InventoryManager::updateObject(const std::string& path,
     dbus.call(call);
 }
 
+void InventoryManager::add(const std::string& path,
+                           const interfaces::Interface iface)
+{
+    ObjectType updates;
+
+    iface.populateObject(updates);
+
+    updateObject(path, updates);
+}
+
+void InventoryManager::remove(const std::string& path,
+                              const interfaces::Interface iface)
+{
+    ObjectType updates;
+
+    iface.depopulateObject(updates);
+
+    updateObject(path, updates);
+}
+
 void InventoryManager::markPresent(const std::string& path)
 {
     std::string absolute = std::string("/xyz/openbmc_project/inventory") + path;
@@ -145,105 +165,4 @@ bool InventoryManager::isModel(const std::string& path,
     }
 
     return false;
-}
-
-/* inventory::accumulate */
-
-void inventory::accumulate(std::map<std::string, ObjectType>& store,
-                           const std::string& path, const ObjectType& updates)
-{
-    if (store.contains(path))
-    {
-        auto& object = store[path];
-
-        for (const auto& [ikey, ival] : updates)
-        {
-            if (object.contains(ikey))
-            {
-                auto& interface = object[ikey];
-
-                for (const auto& [pkey, pval] : ival)
-                {
-                    interface[pkey] = pval;
-                }
-            }
-            else
-            {
-                object[ikey] = ival;
-            }
-        }
-    }
-    else
-    {
-        store[path] = updates;
-    }
-}
-
-/* PublishWhenPresentInventoryDecorator */
-
-PublishWhenPresentInventoryDecorator::PublishWhenPresentInventoryDecorator(
-    Inventory* inventory) :
-    inventory(inventory)
-{}
-
-std::weak_ptr<PropertiesChangedListener>
-    PublishWhenPresentInventoryDecorator::addPropertiesChangedListener(
-        const std::string& path, const std::string& interface,
-        std::function<void(PropertiesChanged&&)> callback)
-{
-    return inventory->addPropertiesChangedListener(path, interface, callback);
-}
-
-void PublishWhenPresentInventoryDecorator::removePropertiesChangedListener(
-    std::weak_ptr<PropertiesChangedListener> listener)
-{
-    inventory->removePropertiesChangedListener(listener);
-}
-
-void PublishWhenPresentInventoryDecorator::updateObject(
-    const std::string& path, const ObjectType& updates)
-{
-    inventory::accumulate(objectCache, path, updates);
-
-    if (presentCache.contains(path) && presentCache[path])
-    {
-        inventory->updateObject(path, objectCache[path]);
-        inventory->markPresent(path);
-    }
-}
-
-void PublishWhenPresentInventoryDecorator::markPresent(const std::string& path)
-{
-    bool alreadyPresent = presentCache.contains(path) && presentCache[path];
-
-    presentCache.insert_or_assign(path, true);
-
-    if (!alreadyPresent && objectCache.contains(path))
-    {
-        inventory->updateObject(path, objectCache[path]);
-        inventory->markPresent(path);
-    }
-}
-
-void PublishWhenPresentInventoryDecorator::markAbsent(const std::string& path)
-{
-    bool wasPresent = !presentCache.contains(path) || presentCache[path];
-
-    presentCache.insert_or_assign(path, false);
-
-    if (wasPresent)
-    {
-        inventory->markAbsent(path);
-    }
-}
-
-bool PublishWhenPresentInventoryDecorator::isPresent(const std::string& path)
-{
-    return inventory->isPresent(path);
-}
-
-bool PublishWhenPresentInventoryDecorator::isModel(const std::string& path,
-                                                   const std::string& model)
-{
-    return inventory->isModel(path, model);
 }

src/inventory/meson.build
@@ -1,0 +1,5 @@
+inventory_src = [
+    'inventory-manager.cpp',
+    'publish-when-present.cpp'
+]
+inventory_dep = declare_dependency(sources: inventory_src)

src/inventory/publish-when-present.cpp
@@ -1,0 +1,112 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright IBM Corp. 2021 */
+#include "dbus.hpp"
+#include "inventory.hpp"
+
+#include <cassert>
+
+using namespace inventory;
+using namespace dbus;
+
+PublishWhenPresentInventoryDecorator::PublishWhenPresentInventoryDecorator(
+    Inventory* inventory) :
+    inventory(inventory)
+{}
+
+std::weak_ptr<PropertiesChangedListener>
+    PublishWhenPresentInventoryDecorator::addPropertiesChangedListener(
+        const std::string& path, const std::string& interface,
+        std::function<void(PropertiesChanged&&)> callback)
+{
+    return inventory->addPropertiesChangedListener(path, interface, callback);
+}
+
+void PublishWhenPresentInventoryDecorator::removePropertiesChangedListener(
+    std::weak_ptr<PropertiesChangedListener> listener)
+{
+    inventory->removePropertiesChangedListener(listener);
+}
+
+void PublishWhenPresentInventoryDecorator::add(
+    const std::string& path, const interfaces::Interface iface)
+{
+    if (!objectCache.contains(path))
+    {
+        objectCache.try_emplace(path);
+    }
+
+    objectCache[path].push_back(iface);
+
+    if (presentCache.contains(path) && presentCache[path])
+    {
+        inventory->add(path, iface);
+    }
+}
+
+void PublishWhenPresentInventoryDecorator::remove(
+    const std::string& path, const interfaces::Interface iface)
+{
+    if (presentCache.contains(path) && !presentCache[path])
+    {
+        inventory->remove(path, iface);
+    }
+
+    if (!objectCache.contains(path))
+    {
+        return;
+    }
+
+    /* Neuter it */
+    ObjectType removed;
+    iface.depopulateObject(removed);
+    auto [k, v] = *(removed.begin());
+    const auto addProps{v};
+    const auto removeProps{v};
+    objectCache[path].emplace_back(k, std::move(addProps),
+                                   std::move(removeProps));
+}
+
+void PublishWhenPresentInventoryDecorator::markPresent(const std::string& path)
+{
+    bool alreadyPresent = presentCache.contains(path) && presentCache[path];
+
+    presentCache.insert_or_assign(path, true);
+
+    if (!alreadyPresent && objectCache.contains(path))
+    {
+        for (const auto& iface : objectCache[path])
+        {
+            inventory->add(path, iface);
+        }
+
+        inventory->markPresent(path);
+    }
+}
+
+void PublishWhenPresentInventoryDecorator::markAbsent(const std::string& path)
+{
+    bool wasPresent = !presentCache.contains(path) || presentCache[path];
+
+    presentCache.insert_or_assign(path, false);
+
+    if (wasPresent && objectCache.contains(path))
+    {
+        inventory->markAbsent(path);
+
+        for (const auto& iface : objectCache[path])
+        {
+            inventory->remove(path, iface);
+        }
+    }
+}
+
+bool PublishWhenPresentInventoryDecorator::isPresent(const std::string& path)
+{
+    return inventory->isPresent(path);
+}
+
+bool PublishWhenPresentInventoryDecorator::isModel(const std::string& path,
+                                                   const std::string& model)
+{
+    return inventory->isModel(path, model);
+}

src/meson.build
@@ -1,6 +1,7 @@
 subdir('sysfs')
 subdir('platforms')
 subdir('devices')
+subdir('inventory')
 
 sdbusplus_dep = dependency('sdbusplus', fallback: [ 'sdbusplus', 'sdbusplus_dep' ])
 phosphor_logging_dep = dependency('phosphor-logging',
@@ -11,9 +12,11 @@ cpp = meson.get_compiler('cpp')
 i2c_dep = cpp.find_library('i2c')
 
 # Used by inventory tests
-inventory_dep = declare_dependency(sources: [ 'inventory.cpp', 'dbus.cpp' ],
+inventory_test_dep = declare_dependency(sources: [ 'dbus.cpp' ],
 				   include_directories: '.',
-				   dependencies: [ sdbusplus_dep, phosphor_logging_dep ])
+				   dependencies: [ inventory_dep,
+						   sdbusplus_dep,
+						   phosphor_logging_dep ])
 
 fru_deps = [ sysfs_dep, platforms_dep, inventory_dep, devices_dep ]
 
@@ -21,7 +24,6 @@ platform_fru_detect_sources = [
     'dbus.cpp',
     'environment.cpp',
     'i2c.cpp',
-    'inventory.cpp',
     'notify.cpp',
     'platform.cpp',
     'platform-fru-detect.cpp',

src/platforms/rainier/flett.cpp
@@ -21,7 +21,10 @@ PHOSPHOR_LOG2_USING;
 FlettNVMeDrive::FlettNVMeDrive(Inventory* inventory, const Nisqually* nisqually,
                                const Flett* flett, int index) :
     BasicNVMeDrive(flett->getDriveBus(index), inventory, index),
-    nisqually(nisqually), flett(flett)
+    nisqually(nisqually), flett(flett),
+    basic(flett->getDriveBus(index).getAddress(), eepromAddress),
+    vini(std::vector<uint8_t>({'N', 'V', 'M', 'e'}),
+         std::move(this->getSerial()))
 {}
 
 void FlettNVMeDrive::plug([[maybe_unused]] Notifier& notifier)
@@ -55,56 +58,21 @@ void FlettNVMeDrive::addToInventory(Inventory* inventory)
 {
     std::string path = getInventoryPath();
 
-    decorateWithI2CDevice(path, inventory);
-    decorateWithVINI(path, inventory);
+    inventory->add(path, basic);
+    inventory->add(path, vini);
 }
 
-void FlettNVMeDrive::removeFromInventory([[maybe_unused]] Inventory* inventory)
+void FlettNVMeDrive::removeFromInventory(Inventory* inventory)
 {
-    debug(
-        "I'm not sure how to remove drive {NVME_ID} on Flett {FLETT_ID} from the inventory!",
-        "NVME_ID", index, "FLETT_ID", flett->getIndex());
+    std::string path = getInventoryPath();
+
+    inventory->remove(path, vini);
+    inventory->remove(path, basic);
 }
 
 bool FlettNVMeDrive::isPresent(SysfsI2CBus bus)
 {
     return bus.isDevicePresent(NVMeDrive::eepromAddress);
-}
-
-void FlettNVMeDrive::decorateWithI2CDevice(const std::string& path,
-                                           Inventory* inventory) const
-{
-    size_t bus = static_cast<size_t>(flett->getDriveBus(index).getAddress());
-    size_t address = static_cast<size_t>(NVMeDrive::eepromAddress);
-
-    inventory::ObjectType updates = {
-        {
-            inventory::INVENTORY_DECORATOR_I2CDEVICE_IFACE,
-            {
-                {"Bus", bus},
-                {"Address", address},
-            },
-        },
-    };
-
-    inventory->updateObject(path, updates);
-}
-
-void FlettNVMeDrive::decorateWithVINI(const std::string& path,
-                                      Inventory* inventory) const
-{
-    inventory::ObjectType updates = {
-        {
-            inventory::INVENTORY_IPZVPD_VINI_IFACE,
-            {
-                {"RT", std::vector<uint8_t>({'V', 'I', 'N', 'I'})},
-                {"CC", std::vector<uint8_t>({'N', 'V', 'M', 'e'})},
-                {"SN", this->getSerial()},
-            },
-        },
-    };
-
-    inventory->updateObject(path, updates);
 }
 
 /* Flett */

commit 938d391ec8b2616769adbc2032ab90c266284403

flett: Remove unused private method declarations
Fixes: 44f393796607 ("inventory: Refactor in terms of interfaces")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

src/platforms/rainier.hpp
@@ -45,10 +45,6 @@ class FlettNVMeDrive : public BasicNVMeDrive
     std::string getInventoryPath() const override;
 
   private:
-    void decorateWithI2CDevice(const std::string& path,
-                               Inventory* inventory) const;
-    void decorateWithVINI(const std::string& path, Inventory* inventory) const;
-
     const Nisqually* nisqually;
     const Flett* flett;
 };